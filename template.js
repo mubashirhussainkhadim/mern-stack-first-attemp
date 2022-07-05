7674bc0da0a2d710f8a6f5f733351781b7f94de1
Mubashir hussain <66620586+mubashirhussainkhadim@users.noreply.github.com>
2022-07-05 21:28:55 +0500

    first commit

---
+++ template.js
@@ -0,0 +1,13 @@
+export default () => {
+    return `<!doctype html>
+     <html lang="en">
+      <head>
+       <meta charset="utf-8">
+       <title>MERN first hello world project </title>
+      </head>
+      <body>
+       <div id="root"></div>
+       <script type="text/javascript" src="/dist/bundle.js">
+      </script>
+      </body>
+</html>` }
